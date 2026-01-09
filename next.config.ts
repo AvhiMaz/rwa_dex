65327187a34536f49a5e310caf0819d3eb8dba54
AvhiMaz <avhimazumder5@outlook.com>
2026-01-09 13:49:41 +0530

    chore: add config
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {
+      "pino-elasticsearch": path.resolve(__dirname, "mocks/pino-elasticsearch.js"),
+      "fastbench": path.resolve(__dirname, "mocks/fastbench.js"),
+    },
+  },
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream", "pino-elasticsearch", "rimraf"],
+  webpack: (config, { webpack }) => {
+    const path = require("path");
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "pino-elasticsearch": path.resolve(__dirname, "mocks/pino-elasticsearch.js"),
+      "fastbench": path.resolve(__dirname, "mocks/fastbench.js"),
+    };
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^(pino-elasticsearch|fastbench|pino-pretty|encoding)$/,
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
